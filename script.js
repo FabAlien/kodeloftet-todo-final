10974e5870ad12c3743106cb0e9b6d9101c39695
FabAlien <fabalien.von@gmail.com>
2024-09-08 22:11:07 +0200

    Final Commit

--- script.js
+++ script.js
@@ -6,3 +6,52 @@ function burgerFunction() {
     x.className = 'navbar';
   }
 }
+
+function textClear() {
+  addTodoButton.addEventListener("click", document.getElementById("todoInput").value = "")
+}
+
+function todoApplication() {
+
+  const todoInput = document.querySelector("#todoInput").value;
+
+  const todoList = document.querySelector("#todoList");
+
+  const todoItem = document.createElement("li");
+
+  todoItem.textContent = todoInput;
+
+  todoItem.classList.add("spacing");
+
+  todoList.appendChild(todoItem);
+
+  const deleteButton = document.createElement("button");
+
+  deleteButton.textContent = "Slett";
+
+  deleteButton.addEventListener("click", function () {
+    todoItem.remove();
+  })
+
+  todoItem.appendChild(deleteButton);
+
+  const finishButton = document.createElement("button");
+
+  finishButton.textContent = "Ferdig";
+
+  finishButton.addEventListener("click", function (){
+    todoItem.style.textDecoration = "line-through";
+    todoItem.style.color = "darkgrey";
+  })
+
+  todoItem.appendChild(finishButton);
+  clearInput;
+
+}
+
+const addTodoButton = document.querySelector('#addTodoButton');
+
+addTodoButton.addEventListener('click', todoApplication);
+addTodoButton.addEventListener("click", textClear)
+
+console.log("JS initiated")
